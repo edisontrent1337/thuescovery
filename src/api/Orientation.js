e05a1719f212bc48df935c67c9d1c965a65b2be6
Johannes Hense <johannes.hense@devboost.de>
2019-06-22 20:57:21 +0200

    Extracted orientation logic to helper class

---
+++ src/api/Orientation.js
@@ -0,0 +1,65 @@
+
+let state = {};
+
+export default class Orientation {
+    static startOrientationListener() {
+        handleOrientation();
+        window.addEventListener('devicemotion', handleAcceleration);
+        window.addEventListener('orientationchange', handleOrientation);
+        state.updateInterval = setInterval(updateOrientation, 16);
+    }
+    
+    static stopOrientationListener() {
+        window.removeEventListener('devicemotion', handleAcceleration);
+        window.removeEventListener('orientationchange', handleOrientation);
+        clearInterval(state.updateInterval);
+    }
+    
+    static resetOrientation() {
+        state.orientation = {alpha: 0, beta: 0, gamma: 0};
+    }
+    
+    static setOrientationListener(listener) {
+        state.listener = listener;
+    }
+}
+
+function handleOrientation(event) {
+    const {orientation} = window;
+    state.landscape = orientation === 90 || orientation === -90;
+}
+
+function handleAcceleration(event) {
+    const acceleration = event.acceleration;
+    state.rotation = event.rotationRate || null;
+    let {x, y, z} = acceleration;
+
+    x = (state.landscape ? y : x);
+    y = (state.landscape ? x : y);
+    z = z * 1;
+
+    state.position = {
+        x, y, z
+    };
+};
+
+
+function updateOrientation() {
+
+    const rotation = state.rotation;
+    if (!rotation) {
+        return;
+    }
+
+    let {alpha, beta, gamma} = state.orientation;
+
+    const FRAME_RATE = 62.5;
+    alpha += rotation.alpha / FRAME_RATE;
+    beta += rotation.beta / FRAME_RATE;
+    gamma += rotation.gamma / FRAME_RATE;
+
+    const orientation = {
+        alpha, beta, gamma
+    };
+    state.orientation = orientation;
+};
--- src/components/DemoScreen/DemoScreen.js
+++ src/components/DemoScreen/DemoScreen.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
-import {compose, withProps} from "recompose";
-import {GoogleMap, Marker, withGoogleMap, withScriptjs} from "react-google-maps";
-import Orientation from "../Orientation/Orientation";
+import React, { Component } from 'react';
+import { compose, withProps } from "recompose";
+import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps";
 import Camera from "react-html5-camera-photo";
+
+import Orientation from '../../api/Orientation';
 
 const API_KEY = "AIzaSyADE1MIp5__mY7JZddAZfHyyGCURkVdAFY";
 
@@ -26,77 +27,24 @@ class DemoScreen extends Component {
     }
 
     componentDidMount() {
-        this.handleOrientation();
-        window.addEventListener('devicemotion', this.handleAcceleration);
-        window.addEventListener('orientationchange', this.handleOrientation);
-        this.updateInterval = setInterval(this.updateOrientation, 16)
-
+        Orientation.startOrientationListener();
+        Orientation.setOrientationListener((orientation) => {
+            this.setState({ orientation: orientation });
+        });
     }
 
     componentWillUnmount() {
-        window.removeEventListener('devicemotion', this.handleAcceleration);
-        window.removeEventListener('orientationchange', this.handleOrientation);
-        clearInterval(this.updateInterval);
+        Orientation.stopOrientationListener();
     }
 
-    handleOrientation = (event) => {
-        const {orientation} = window;
-        this.setState({landscape: orientation === 90 || orientation === -90})
-    };
-
-    handleAcceleration = (event) => {
-        const {landscape} = this.state;
-        const {useGravity} = this.props;
-        const acceleration = useGravity ? event.accelerationIncludingGravity : event.acceleration;
-        const rotation = event.rotationRate || null;
-        let {x, y, z} = acceleration;
-
-        x = (landscape ? y : x);
-        y = (landscape ? x : y);
-        z = z * 1;
-
-        const position = {
-            x, y, z
-        };
-
-        this.setState({
-            rotation,
-            position
-        })
-    };
-
-
-    updateOrientation = () => {
-
-        const rotation = this.state.rotation;
-        if (!rotation) {
-            return;
-        }
-
-        let {alpha, beta, gamma} = this.state.orientation;
-
-        const FRAME_RATE = 62.5;
-        alpha += rotation.alpha / FRAME_RATE;
-        beta += rotation.beta / FRAME_RATE;
-        gamma += rotation.gamma / FRAME_RATE;
-
-        const orientation = {
-            alpha, beta, gamma
-        };
-        this.setState({
-            orientation
-        });
-    };
-
     render() {
-        const {position, rotation, orientation} = this.state;
+        const { orientation } = this.state;
         return (
             <div>
-                <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
-                    <Marker position={{lat: -34.397, lng: 150.644}}/>
-                    <Marker position={{lat: -34.392, lng: 150.644}}/>
+                <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+                    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+                    <Marker position={{ lat: -34.392, lng: 150.644 }} />
                 </GoogleMap>
-                <Orientation/>
                 <Camera
                     idealFacingMode={'environment'}
                     isImageMirror={false}
@@ -106,48 +54,12 @@ class DemoScreen extends Component {
                 />
                 <div>
 
-                    {position && <div style={{transform: `translate3d(${position.x}px, ${position.y}px, 0)`}}>
+                    {orientation && <div style={{ transform: `translate3d(${orientation.alpha}px, ${orientation.beta}px, 0)` }}>
                         Hello there
                     </div>}
 
                     {orientation && <div>{JSON.stringify(orientation)}</div>}
 
-                    {position &&
-                    <div style={{
-                        height: '20px',
-                        width: 8 * position.x + 'px',
-                        backgroundColor: 'red'
-                    }}>{position.x}</div>}
-                    {position && <div
-                        style={{
-                            height: '20px',
-                            width: 8 * position.y + 'px',
-                            backgroundColor: 'blue'
-                        }}>{position.y}</div>}
-                    {position && <div style={{
-                        height: '20px',
-                        width: 8 * position.z + 'px',
-                        backgroundColor: 'green'
-                    }}>{position.z}</div>}
-                    {rotation && <div
-                        style={{
-                            height: '20px',
-                            width: rotation.alpha + 'px',
-                            backgroundColor: 'pink'
-                        }}>rotation
-                        alpha: {rotation && rotation.alpha}</div>}
-                    {rotation && <div style={{
-                        height: '20px',
-                        width: rotation.beta + 'px',
-                        backgroundColor: 'yellow'
-                    }}>rotation beta: {rotation && rotation.beta}</div>}
-                    {rotation && <div
-                        style={{
-                            height: '20px',
-                            width: rotation.gamma + 'px',
-                            backgroundColor: 'cyan'
-                        }}>rotation
-                        gamma: {rotation && rotation.gamma}</div>}
                 </div>
             </div>);
     }
@@ -162,9 +74,9 @@ export default compose(
          */
         googleMapURL:
             "https://maps.googleapis.com/maps/api/js?key=" + API_KEY + "&v=3.exp&libraries=geometry,drawing,places",
-        loadingElement: <div style={{height: `100%`}}/>,
-        containerElement: <div style={{height: `400px`}}/>,
-        mapElement: <div style={{height: `100%`}}/>,
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
         useGravity: true
     }),
     withScriptjs,
