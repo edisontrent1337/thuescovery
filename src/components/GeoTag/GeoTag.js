56fb4181d2534ccf1042ca60b4cc9c2e93a67d24
Sinthujan <sthana@devboost.de>
2019-06-23 00:16:00 +0200

    Enhanced POI ARView.

--- src/components/GeoTag/GeoTag.js
+++ src/components/GeoTag/GeoTag.js
@@ -1,9 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import './GeoTag.css';
 
 class GeoTag extends Component {
-  render() {
-    return <div>GeoTag</div>;
-  }
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const {orientation, position, poi} = this.props;
+        const scale = 15;
+        return <div className="tag" style={{
+            top: position.y,
+            left: position.x,
+            transform: `translate3d(${orientation.beta * scale}px, ${orientation.alpha * scale}px,0)`
+        }}>
+            <div><span className={'mdi mdi-' + this.determineIcon(poi)}>{poi.name}</span></div>
+
+            {poi.photo && <img src={poi.photo}/>}
+        </div>;
+    }
+
+
+    determineIcon = (poi) => {
+        console.log(poi.additionaleType);
+        const iconTypes = {
+            'Museum': 'bank',
+            'Sakral': 'church',
+            'Restaurant': 'food-fork-drink',
+            'Theater': 'drama-masks',
+            'Botanischer Garten': 'flower-outline',
+            'St&auml;ndiges Orchester': 'music-note',
+            'Kulturhaus': 'school'
+        };
+
+        return iconTypes[poi.additionaleType];
+
+    }
 }
 
 export default GeoTag;
--- src/components/screens/ARScreen/ARScreen.js
+++ src/components/screens/ARScreen/ARScreen.js
@@ -1,16 +1,83 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Camera from "react-html5-camera-photo";
+import getLocation from "../../../api/GeoLocation";
+import ThueCAT from "../../../api/ThueCAT";
+import Container from "react-bootstrap/Container";
+import GeoTag from "../../GeoTag/GeoTag";
+import Orientation from "../../../api/Orientation";
 
-class ARScreen extends Component {
+class ARScreen extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        const API_KEY = "z24f1FQAcnRml4tkmXca4lcFKF.koAsc4KCNWGO75nsLAeifNNkwgLF73zrnscb3dfnZdbk3mm.SPAl2DJFIoOz2evCHwflUF4";
+        this.api = new ThueCAT(API_KEY);
+        this.state = {
+            pointsOfInterest: [],
+            positions: [],
+            orientation: {
+                alpha: 0,
+                beta: 0,
+                gamma: 0,
+            },
+        };
+    }
+
+    componentDidMount() {
+        Orientation.startOrientationListener();
+        Orientation.setOrientationListener((orientation) => {
+            this.setState({orientation: orientation});
+        });
+
+    }
+
+    componentWillUnmount() {
+        Orientation.stopOrientationListener();
+    }
+
+
+    async componentWillMount() {
+        // fetch current location
+        let position = await getLocation();
+
+        this.setState({position: 'Got position: ' + JSON.stringify(position.coords)});
+        // search around it
+        let response = await this.api.geoSearch(position.latitude, position.longitude, 10000);
+        this.generateRandomPositions(response);
+        this.setState({pointsOfInterest: response});
+    }
+
+
+    generateRandomPositions = (response) => {
+        let positions = [];
+        response.forEach(() => {
+            positions.push({x: (-0.5 + Math.random()) * 3000, y: Math.random() * 400})
+        });
+        this.setState({
+            positions
+        }, () => console.log(response));
+    };
+
     render() {
+
+        const geoTags = this.state.pointsOfInterest.map((poi, key) => {
+            return <GeoTag poi={poi} key={key} position={this.state.positions[key]}
+                           orientation={this.state.orientation}/>;
+        });
+
         return (
-            <Camera
-                idealFacingMode={'environment'}
-                isImageMirror={false}
-                onTakePhoto={(dataUri) => {
-                    this.onTakePhoto(dataUri);
-                }}
-            />
+            <Container>
+                <div style={{marginLeft: '-15px', position: 'relative'}}>
+                    {geoTags}
+                    <Camera
+                        idealFacingMode={'environment'}
+                        isImageMirror={false}
+                        onTakePhoto={(dataUri) => {
+                            this.onTakePhoto(dataUri);
+                        }}
+                    />
+                </div>
+            </Container>
         );
     }
 }
