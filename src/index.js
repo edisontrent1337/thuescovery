17f943ba73da5f279b746f4cb18737055046f5fc
Sinthujan <sthana@devboost.de>
2019-06-22 17:33:49 +0200

    Added CameraView and Map

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {compose, withProps} from "recompose";
+import Camera from 'react-html5-camera-photo';
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker
+} from "react-google-maps";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function onTakePhoto(){
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}
+
+let API_KEY = "AIzaSyADE1MIp5__mY7JZddAZfHyyGCURkVdAFY";
+const MyMapComponent = compose(
+    withProps({
+        /**
+         * Note: create and replace your own key in the Google console.
+         * https://console.developers.google.com/apis/dashboard
+         * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
+         */
+        googleMapURL:
+            "https://maps.googleapis.com/maps/api/js?key=" + API_KEY + "&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{height: `100%`}}/>,
+        containerElement: <div style={{height: `400px`}}/>,
+        mapElement: <div style={{height: `100%`}}/>
+    }),
+    withScriptjs,
+    withGoogleMap
+)(props => (
+    <div>
+        <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
+            <Marker position={{lat: -34.397, lng: 150.644}}/>
+            <Marker position={{lat: -34.392, lng: 150.644}}/>
+        </GoogleMap>
+        <Camera
+            idealFacingMode={'environment'}
+            isImageMirror={false}
+            onTakePhoto={(dataUri) => {
+                this.onTakePhoto(dataUri);
+            }}
+        />
+    </div>
+));
+
+ReactDOM.render(<MyMapComponent isMarkerShown/>, document.getElementById("root"));
